a4058cebab87c917a01355bb7f339e13d7d47d34
linmao0991 <lin.maoxian0991@gmail.com>
2020-11-23 05:25:04 -0500

    Added new bio image. Put about me card in its own column and all the projects in another column. Making bio and project cards not stacking

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     "development": {
-      "username": process.env.MYSQL_USER,
-      "password": process.env.MYSQL_KEY,
-      "database": process.env.MYSQL_DBNAME,
-      "host": process.env.MYSQL_HOST,
+      "username": 'xvex5lt7dygr71y5',
+      "password": 'srn5cjh930ao8irk',
+      "database": 'oveynqztnucgnq5x',
+      "host": 'z37udk8g6jiaqcbx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
       "dialect": "mysql",
     },
     "test": {
